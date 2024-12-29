7164469c162bdc9964d2602ad6b1568c9352ba87
Reza Mansouri <itsrezamansouri@gmail.com>
2024-12-28 23:02:24 -0500

    bugfix

--- src/dataset/yolo_labeler.py
+++ src/dataset/yolo_labeler.py
@@ -8,13 +8,13 @@ def main():
     hek_labels_path = sys.argv[1]
     hek_labels_names = os.listdir(hek_labels_path)
     now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    os.mkdir(f"{now}-yolo-labels")
+    new_labels_path = os.path.join(hek_labels_path, "..", f"{now}-yolo-labels")
     for hek_label_name in hek_labels_names:
         df = pd.read_csv(os.path.join(hek_labels_path, hek_label_name))
         if len(df) == 0:
             pass
         with open(
-            os.path.join(f"{now}-yolo-labels", hek_label_name[:-3] + "txt"),
+            os.path.join(new_labels_path, hek_label_name[:-3] + "txt"),
             "w",
             encoding="utf-8",
         ) as txt_label:
@@ -26,7 +26,7 @@ def main():
                     row["height"],
                 )
                 center_x, center_y = min_x + width / 2, min_y + height / 2
-                center_x, center_y, min_x, min_y = [
+                center_x, center_y, width, height = [
                     i / 4096 for i in [center_x, center_y, width, height]
                 ]
                 txt_label.write(f"0 {center_x} {center_y} {width} {height}\n")
