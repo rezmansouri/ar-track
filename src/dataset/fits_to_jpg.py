396a33904a62bb8bff34dfc178fb840634c318cf
Reza Mansouri <itsrezamansouri@gmail.com>
2025-01-01 19:01:50 -0500

    update

---
+++ src/dataset/fits_to_jpg.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import cv2 as cv
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from astropy.io import fits
+from datetime import datetime
+
+_mask_rad = 1900
+MASK = np.zeros((4096, 4096), dtype=np.uint8)
+cv.circle(MASK, (2048, 2048), _mask_rad, 1, thickness=-1)
+MASK = MASK.astype(bool)
+
+
+def preprocess_log_minmax(los_magnetogram, size=4096):
+    """
+    todo
+    """
+    nan_ix = np.isnan(los_magnetogram)
+    los_magnetogram[nan_ix] = np.average(los_magnetogram[~nan_ix])
+    biased_data = np.abs(los_magnetogram) + 1
+
+    log_scaled_data = np.log(biased_data)
+
+    log_scaled_data[los_magnetogram < 0] *= -1
+    final = np.zeros_like(los_magnetogram, dtype=np.float32)
+    x_min, x_max = log_scaled_data[MASK].min(), log_scaled_data[MASK].max()
+    final[MASK] = (log_scaled_data[MASK] - x_min) / (x_max - x_min)
+    final = np.array(final * 255, dtype=np.uint8)
+    return final
+
+
+def convert(fits_path, jpg_path):
+    hdul = fits.open(fits_path)
+    data = hdul[1].data
+    arr = preprocess_log_minmax(data)
+    image = Image.fromarray(arr)
+    image.save(jpg_path)
+
+
+def main():
+    fits_paths = sys.argv1[1]
+    fits_names = os.listdir(fits_paths)
+    time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    jpgs_paths = os.path.join(fits_paths, "..", f"jpg-{time}")
+    for fits_name in tqdm(fits_names):
+        fits_path = os.path.join(fits_paths, fits_name)
+        jpg_path = os.path.join(jpgs_paths, fits_name[:-4] + "jpg")
+        convert(fits_path, jpg_path)
+
+
+if __name__ == "__main__":
+    main()
--- src/dataset/preprocess.py
+++ src/dataset/preprocess.py
@@ -5,10 +5,35 @@ todo
 import cv2 as cv
 import numpy as np
 
-_mask_rad = 1950
+_mask_rad = 1900
 MASK = np.zeros((4096, 4096), dtype=np.uint8)
 cv.circle(MASK, (2048, 2048), _mask_rad, 1, thickness=-1)
 MASK = MASK.astype(bool)
+
+
+def preprocess_log_minmax_test_rad(los_magnetogram, rad=1900, size=4096):
+    """
+    todo
+    """
+    
+    mask = np.zeros((4096, 4096), dtype=np.uint8)
+    cv.circle(mask, (2048, 2048), rad, 1, thickness=-1)
+    mask = mask.astype(bool)
+    nan_ix = np.isnan(los_magnetogram)
+    los_magnetogram[nan_ix] = np.average(los_magnetogram[~nan_ix])
+    biased_data = np.abs(los_magnetogram) + 1
+
+    log_scaled_data = np.log(biased_data)
+
+    log_scaled_data[los_magnetogram < 0] *= -1
+    final = np.zeros_like(los_magnetogram, dtype=np.float32)
+    x_min, x_max = log_scaled_data[mask].min(), log_scaled_data[mask].max()
+    final[mask] = (log_scaled_data[mask] - x_min) / (x_max - x_min)
+    if size != 4096:
+        final = np.array(final * 255, dtype=np.uint8)
+        final = cv.resize(final, (size, size), interpolation=cv.INTER_CUBIC)
+        final = np.array(final, dtype=np.float32) / 255
+    return final
 
 
 def preprocess_log_minmax(los_magnetogram, size=4096):
