6c345d8be36731965794b3d09bf2b43c04ea38b3
Reza Mansouri <itsrezamansouri@gmail.com>
2025-01-09 23:01:12 -0500

    validate

---
+++ src/detector/yolov11/ultralytics/validate.py
@@ -0,0 +1,35 @@
+import sys
+from ultralytics import YOLO
+
+
+def main():
+    image_size, batch_size, scale, devices = (
+        int(sys.argv[1]),
+        int(sys.argv[2]),
+        sys.argv[3],
+        sys.argv[4],
+    )
+    devices = [int(d) for d in devices]
+    model = YOLO(f"./yolo11{scale}.yaml")
+
+    iou_threshs = range(0.05, 0.95, 0.05)
+    conf_threshs = range(0.05, 0.95, 0.05)
+    for iou in iou_threshs:
+        for conf in conf_threshs:
+            metrics = model.val(
+                data="/home/rmansouri1/ar_track/ar-track/src/detector/yolov11/ultralytics/hek.yaml",
+                imgsz=image_size,
+                batch=batch_size,
+                save_json=True,
+                conf=conf,
+                iou=iou,
+                plots=True,
+                split="val",
+                device=devices,
+                name=f"{scale}conf-{conf}-iou{iou}",
+            )
+            print(f"conf: {conf} iou: {iou} mAP50-95: {metrics.map}")
+
+
+if __name__ == "__main__":
+    main()
